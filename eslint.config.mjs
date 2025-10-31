fbe29c70908f60a767762fd654c6378c50c3ee40
godlikeAlex <godlikedesigner@gmail.com>
2025-10-31 23:51:12 +0500

    chore: add basic commitlint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,6 +6,11 @@ const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
   globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
+  {
+    rules: {
+      allowObject: true,
+    },
+  },
 ]);
 
 export default eslintConfig;
