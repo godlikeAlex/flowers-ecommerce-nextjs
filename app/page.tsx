e94c68c7052c499686b9358e6529099b77461583
godlikeAlex <godlikedesigner@gmail.com>
2025-11-01 01:40:18 +0500

    chore: add eslint rules

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
+import { useEffect, useState } from "react";
+
 export default function Home() {
+  const [state, setState] = useState();
+
+  useEffect(() => {
+    console.log(state);
+  }, []);
+
   return <main>Flowers Shop</main>;
 }
