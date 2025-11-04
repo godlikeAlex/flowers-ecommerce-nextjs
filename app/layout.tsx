72779436f974e5f7084a58c5a262eaa4e98734bc
godlikeAlex <godlikedesigner@gmail.com>
2025-11-04 15:08:10 +0500

    feat: init font with nextjs

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,27 @@
+import localFont from "next/font/local";
 import type { Metadata } from "next";
 
 import "@/app/global.css";
+
+const bluemelleFont = localFont({
+  src: [
+    {
+      path: "../public/fonts/bluemelle-regular.otf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/bluemelle-bold.otf",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/bluemelle-black.otf",
+      weight: "700",
+      style: "normal",
+    },
+  ],
+});
 
 export const metadata: Metadata = {
   title: "BLUEMELLE - Flower boutique",
@@ -13,7 +34,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={bluemelleFont.className}>
       <body>{children}</body>
     </html>
   );
