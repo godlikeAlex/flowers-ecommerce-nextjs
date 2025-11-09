933d01f734bf7254fd820b49bc1b115ae55f80b4
godlikeAlex <godlikedesigner@gmail.com>
2025-11-09 23:15:54 +0500

    feat: add CardCategory and section for homepage

--- src/pages/home/ui/Home/Home.tsx
+++ src/pages/home/ui/Home/Home.tsx
@@ -1,12 +1,13 @@
 import { HeroSection } from "../HeroSection";
 import { AdvantagesSection } from "../AdvantagesSection";
+import { FeaturedCategoriesSection } from "../FeaturedCategoriesSection";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
-
       <AdvantagesSection />
+      <FeaturedCategoriesSection />
     </>
   );
 }
