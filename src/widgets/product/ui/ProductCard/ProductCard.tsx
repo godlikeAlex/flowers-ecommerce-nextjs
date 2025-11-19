643fd9af404c76ac9a5387b5a4ddccbec4d5c2b1
godlikeAlex <godlikedesigner@gmail.com>
2025-11-19 12:58:19 +0500

    feat: add priceformat for product card and hover effect to image

--- src/widgets/product/ui/ProductCard/ProductCard.tsx
+++ src/widgets/product/ui/ProductCard/ProductCard.tsx
@@ -6,14 +6,17 @@ import { useState } from "react";
 
 import { ShoppingCartSimpleIcon } from "@phosphor-icons/react/dist/ssr/ShoppingCartSimple";
 
-import { IconButton, QuantityControl } from "@/shared/ui";
+import { Anchor, IconButton, QuantityControl } from "@/shared/ui";
 
 import styles from "./ProductCard.module.css";
 import Image from "next/image";
+import { ROUTES } from "@/shared/config";
+import { formatPrice } from "@/shared/lib";
 
 interface Props {
   title: string;
   description: string;
+  slug: string;
   image: string;
   price: number;
 }
@@ -23,10 +26,11 @@ export default function ProductCard({
   description,
   price,
   image,
+  slug,
 }: Props) {
   const [quantity, setQuantity] = useState(1);
 
-  const localPrice = (price / 100).toLocaleString();
+  const localPrice = formatPrice(price);
 
   const onIncrement = () => setQuantity((quantity) => Math.max(quantity + 1));
   const onDecrement = () => {
@@ -49,26 +53,34 @@ export default function ProductCard({
 
   return (
     <article className={styles["product-card"]}>
-      <figure className={styles.figure}>
-        <Image
-          src={image}
-          alt={title}
-          fill
-          sizes="(max-width: 768px) 100vw, 700px"
-          style={{ objectFit: "cover" }}
-        />
-      </figure>
+      <Link href={ROUTES.PRODUCT(slug)}>
+        <figure className={styles.figure}>
+          <Image
+            src={image}
+            alt={title}
+            fill
+            sizes="(max-width: 768px) 100vw, 700px"
+            style={{ objectFit: "cover" }}
+          />
+        </figure>
+      </Link>
+
       <div className={styles["text-block"]}>
         <div>
-          <Link href="product-detail.html" className="h4 mb-16">
+          <Anchor
+            as={Link}
+            variant="basic"
+            href={ROUTES.PRODUCT(slug)}
+            className="h4 mb-16"
+          >
             {title}
-          </Link>
+          </Anchor>
           <p className="mb-24">{description}</p>
         </div>
 
         <div>
           <div className={clsx(styles.price, "mb-32")}>
-            <h3>${localPrice}</h3>
+            <h3>{localPrice}</h3>
           </div>
           <div className={styles["action-block"]}>
             <QuantityControl
