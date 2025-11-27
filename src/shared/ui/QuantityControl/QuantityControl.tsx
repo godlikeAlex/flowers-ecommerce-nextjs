c664a2d210ff00c88522f71e9113256486734d7f
godlikeAlex <godlikedesigner@gmail.com>
2025-11-27 14:30:39 +0500

    feat: add support custom class for quantity controll

--- src/shared/ui/QuantityControl/QuantityControl.tsx
+++ src/shared/ui/QuantityControl/QuantityControl.tsx
@@ -5,14 +5,15 @@ import { MinusIcon } from "@phosphor-icons/react/dist/ssr/Minus";
 
 import styles from "./QuantityControl.module.css";
 import { ChangeEvent } from "react";
+import clsx from "clsx";
 
 interface Props {
   value: number;
+  disabled?: boolean;
   classNames?: {
     wrapper?: string;
     decrement?: string;
     increment?: string;
-    input?: string;
   };
   onIncrement: () => void;
   onDecrement: () => void;
@@ -22,10 +23,12 @@ interface Props {
 
 export default function QuantityControl({
   value,
+  disabled,
   onIncrement,
   onDecrement,
   onChange,
   onBlur,
+  classNames,
 }: Props) {
   const handleChangeQuantity = (e: ChangeEvent<HTMLInputElement>) => {
     const { value } = e.target;
@@ -40,11 +43,12 @@ export default function QuantityControl({
   };
 
   return (
-    <div className={styles["quantity-wrap"]}>
+    <div className={clsx(styles["quantity-wrap"], classNames?.wrapper)}>
       <button
         onClick={onDecrement}
-        className={styles.decrement}
+        className={clsx(styles.decrement, classNames?.decrement)}
         aria-label="Decrement"
+        disabled={disabled}
       >
         <MinusIcon />
       </button>
@@ -57,12 +61,14 @@ export default function QuantityControl({
         onChange={handleChangeQuantity}
         className="number"
         onBlur={onBlur}
+        disabled={disabled}
       />
 
       <button
         aria-label="Increment"
         onClick={onIncrement}
-        className={styles.increment}
+        className={clsx(styles.increment, classNames?.increment)}
+        disabled={disabled}
       >
         <PlusIcon />
       </button>
