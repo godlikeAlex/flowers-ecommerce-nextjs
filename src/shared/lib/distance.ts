e5b847af30e5f7dceb0eb6de78b052ddb38f699c
godlikeAlex <godlikedesigner@gmail.com>
2025-11-29 01:15:03 +0500

    feat: add libs and config for geo

---
+++ src/shared/lib/distance.ts
@@ -0,0 +1,4 @@
+export const milesToKm = (miles: number): number => miles * 1.60934;
+export const kmToMiles = (km: number): number => km / 1.60934;
+export const milesToMeters = (miles: number): number => miles * 1609.34;
+export const metersToMiles = (meters: number): number => meters / 1609.34;
