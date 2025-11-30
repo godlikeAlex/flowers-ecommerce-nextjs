f979e695f38ff94356d812d1b72dc847267a2604
godlikeAlex <godlikedesigner@gmail.com>
2025-12-01 01:09:57 +0500

    feat: add Slider navigation center flag and add to review

--- src/pages/home/ui/SliderNavigation/SliderNavigation.tsx
+++ src/pages/home/ui/SliderNavigation/SliderNavigation.tsx
@@ -3,11 +3,19 @@ import { ArrowLeftIcon } from "@phosphor-icons/react/dist/ssr/ArrowLeft";
 import { ArrowRightIcon } from "@phosphor-icons/react/dist/ssr/ArrowRight";
 
 import styles from "./SliderNavigation.module.css";
+import clsx from "clsx";
 
-export default function SliderNavigation() {
+interface Props {
+  center?: boolean;
+}
+
+export default function SliderNavigation({ center }: Props) {
   return (
     <Carousel.Navigation
-      containerClassName={styles.navigation}
+      containerClassName={clsx(
+        styles.navigation,
+        center && styles["navigation--center"],
+      )}
       nextButtonClassName={styles.buttonArrow}
       prevButtonClassName={styles.buttonArrow}
       nextButtonContent={<ArrowRightIcon weight="bold" />}
