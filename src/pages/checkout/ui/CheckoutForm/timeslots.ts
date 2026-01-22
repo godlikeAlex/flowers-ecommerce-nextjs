86b52ea223e2f33ea79b6dd6fe97b48656a2e3c4
godlikeAlex <godlikedesigner@gmail.com>
2026-01-22 13:10:07 +0500

    feat: add timeslots component

---
+++ src/pages/checkout/ui/CheckoutForm/timeslots.ts
@@ -0,0 +1,21 @@
+export const DELIVERY_INTERVALS = [
+  {
+    label: "9:00 AM – 1:00 PM",
+    time: "09:00",
+  },
+  {
+    label: "1:00 PM – 4:00 PM",
+    time: "13:00",
+  },
+];
+
+export const PICKUP_INTERVALS = [
+  {
+    label: "11:00 AM – 1:00 PM",
+    time: "11:00",
+  },
+  {
+    label: "1:00 PM – 4:00 PM",
+    time: "13:00",
+  },
+];
