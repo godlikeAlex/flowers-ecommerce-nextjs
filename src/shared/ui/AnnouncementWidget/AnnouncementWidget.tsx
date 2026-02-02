73dd8b75aaf3ec4ab2649aab7818c37af8a9d9b6
godlikeAlex <godlikedesigner@gmail.com>
2026-02-02 12:21:25 +0500

    feat: add lazy to widgets

--- src/shared/ui/AnnouncementWidget/AnnouncementWidget.tsx
+++ src/shared/ui/AnnouncementWidget/AnnouncementWidget.tsx
@@ -3,5 +3,7 @@
 import { ElfsightWidget } from "react-elfsight-widget";
 
 export default function AnnouncementWidget() {
-  return <ElfsightWidget widgetId="354575cd-0d97-422a-b51a-2a1c8efa97f6" />;
+  return (
+    <ElfsightWidget widgetId="354575cd-0d97-422a-b51a-2a1c8efa97f6" lazy />
+  );
 }
--- src/shared/ui/WhatsAppWidget/WhatsAppWidget.tsx
+++ src/shared/ui/WhatsAppWidget/WhatsAppWidget.tsx
@@ -3,5 +3,7 @@
 import { ElfsightWidget } from "react-elfsight-widget";
 
 export default function WhatsAppWidget() {
-  return <ElfsightWidget widgetId="3a1bc87e-aaf9-41c7-880b-396e47b14570" />;
+  return (
+    <ElfsightWidget widgetId="3a1bc87e-aaf9-41c7-880b-396e47b14570" lazy />
+  );
 }
